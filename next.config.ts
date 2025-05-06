126ab69aec5d1d55ddce0289e64c9f68a2662672
Rahman Shafiee <r.shafiee@gmx.de>
2025-05-06 15:46:54 +0200

    Initial working version of Next.js app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // This creates a standalone build optimized for Node.js servers
 };
 
-export default nextConfig;
+module.exports = nextConfig;
